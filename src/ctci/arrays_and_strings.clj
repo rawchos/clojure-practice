d947885c6b0cb5b248482b061096f1e5e28df47f
Neal Scott <nealnmel@gmail.com>
2020-11-29 11:37:16 -0600

    Check for string permutations

--- src/ctci/arrays_and_strings.clj
+++ src/ctci/arrays_and_strings.clj
@@ -1,8 +1,23 @@
-(ns ctci.arrays-and-strings)
+(ns ctci.arrays-and-strings
+  (:require [clojure.string :as s]))
 
 ;; Question 1.1; Determine if a string passed in is all
 ;; unique characters.
 (defn is-unique? [string]
-  (let [char-set (set string)]
+  (let [low-string (s/lower-case string)
+        char-set (set low-string)]
     (= (count char-set)
-       (count string))))
+       (count low-string))))
+
+;; Question 1.2: Given two strings, write a method to determine if
+;; one is a permutation of the other.
+(defn lower-freqs [string]
+  (frequencies (s/lower-case string)))
+
+(defn is-permutation? 
+  "Check's to see if the strings are permutations of each other by
+   first converting to lower case, then comparing the frequencies of
+   each character. This method doesn't do anything with spaces."
+  [first-string second-string]
+  (= (lower-freqs first-string)
+     (lower-freqs second-string)))
--- test/ctci/arrays_and_strings_test.clj
+++ test/ctci/arrays_and_strings_test.clj
@@ -4,11 +4,28 @@
                                  facts
                                  =>]]))
 
-(facts "about 'is-unique?"
+(facts "about 'is-unique?'"
        (fact "should be true if all characters are unique"
              (aas/is-unique? "blah") => true
              (aas/is-unique? "abcdef") => true)
        (fact "should be false if all characters are not unique"
              (aas/is-unique? "bblah") => false
              (aas/is-unique? "bllahh") => false
-             (aas/is-unique? "blahdeblah") => false))
+             (aas/is-unique? "blahdeblah") => false)
+       (fact "should ignore case"
+             (aas/is-unique? "Bblah") => false
+             (aas/is-unique? "blAah") => false
+             (aas/is-unique? "blaHH") => false))
+
+(facts "about 'is-permutation?'"
+       (fact "should be true if the strings are permutations"
+             (aas/is-permutation? "blah" "albh") => true
+             (aas/is-permutation? "abcdefg" "gfeabcd") => true)
+       (fact "should be false if the strings aren't permutations"
+             (aas/is-permutation? "abcd" "efgh") => false
+             (aas/is-permutation? "abcd" "abcde") => false
+             (aas/is-permutation? "abcd" "abzcd") => false)
+       (fact "should ignore case"
+             (aas/is-permutation? "abcd" "aBcd") => true
+             (aas/is-permutation? "ABCD" "abcd") => true
+             (aas/is-permutation? "ABCD" "ABzCD") => false))
