d22c84f379a7ec76d2e0a4949f402e5cc6942ad8
Neal Scott <nealnmel@gmail.com>
2020-12-13 12:43:48 -0600

    Add solution for part 1 of day 1 AoC-2020

--- test/aoc_20/day_1_test.clj
+++ test/aoc_20/day_1_test.clj
@@ -3,3 +3,7 @@
                                  facts
                                  =>]]
             [aoc-20.day-1 :as d1]))
+
+(facts "about 'part-1'"
+       (fact "should mulitiply the 2 numbers that sum up to 2020"
+             (d1/part-1 [1721 979 366 299 675 1456]) => 514579))
