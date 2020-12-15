96303dffa5973f2debcf27f82ea368ec961cbe5b
Neal Scott <nealnmel@gmail.com>
2020-12-14 18:36:45 -0600

    Add solution for part 2 of day 2 AoC 2020

--- src/aoc_20/day_2.clj
+++ src/aoc_20/day_2.clj
@@ -59,9 +59,63 @@
            0))
        (reduce +))) 
 
+;; --- Part Two ---
+;; While it appears you validated the passwords correctly, they don't seem
+;; to be what the Official Toboggan Corporate Authentication System is
+;; expecting.
+;;
+;; The shopkeeper suddenly realizes that he just accidentally explained the
+;; password policy rules from his old job at the sled rental place down the
+;; street! The Official Toboggan Corporate Policy actually works a little
+;; differently.
+;;
+;; Each policy actually describes two positions in the password, where 1
+;; means the first character, 2 means the second character, and so on.
+;; (Be careful; Toboggan Corporate Policies have no concept of "index zero"!)
+;; Exactly one of these positions must contain the given letter. Other
+;; occurrences of the letter are irrelevant for the purposes of policy
+;; enforcement.
+;;
+;; Given the same example list from above:
+;;
+;; 1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+;; 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+;; 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+;;
+;; How many passwords are valid according to the new interpretation of the
+;; policies?
+
+(defn char-at 
+  "Returns the character found in the position specified. Assumes no zero
+   index."
+  [string position]
+  (if (or (> position (count string))
+          (= 0 position))
+    nil
+    (str (nth string (dec position)))))
+
+(defn valid-positions? [password letter p1 p2]
+  (let [first-char  (char-at password p1)
+        second-char (char-at password p2)]
+    (cond
+      (= first-char second-char letter) false
+      (= first-char letter) true
+      (= second-char letter) true
+      :else false)))
+
+(defn part-2 [input]
+  (->> (for [line input
+             :let [[p1 p2 letter _ password] (-> (s/replace line #"[-:]" " ")
+                                                 (s/split #" "))]]
+         (when (valid-positions? password letter (to-int p1) (to-int p2))
+           true))
+       (remove nil?)
+       (count)))
+
 (def day2-example-input ["1-3 a: abcde"
                          "1-3 b: cdefg"
                          "2-9 c: ccccccccc"])
 
 ; (util/read-lines "day2-input.txt")
 ; (part-1 (util/read-lines "day2-input.txt"))
+; (part-2 (util/read-lines "day2-input.txt"))
--- test/aoc_20/day_2_test.clj
+++ test/aoc_20/day_2_test.clj
@@ -24,3 +24,25 @@
              (d2/part-1 ["1-3 a: abcde"
                          "1-3 b: cdefg"
                          "2-9 c: ccccccccc"]) => 2))
+
+(facts "about 'char-at'"
+       (fact "returns the character at the correct index (non-zero based)"
+             (d2/char-at "blah" 1) => "b"
+             (d2/char-at "blah" 4) => "h"
+             (d2/char-at "blah" 5) => nil
+             (d2/char-at "blah" 0) => nil))
+
+(facts "about 'valid-positions?'"
+       (fact "true if the character is at either position"
+             (d2/valid-positions? "abcde" "a" 1 3) => true
+             (d2/valid-positions? "cbade" "a" 1 3) => true)
+       (fact "false if the character is in neither position"
+             (d2/valid-positions? "cdefg" "b" 1 3) => false)
+       (fact "false if the character is in both positions"
+             (d2/valid-positions? "ccccccccc" "c" 2 9) => false))
+
+(facts "about 'part-2'"
+       (fact "should count the number of valid passwords"
+             (d2/part-2 ["1-3 a: abcde"
+                         "1-3 b: cdefg"
+                         "2-9 c: ccccccccc"]) => 1))
