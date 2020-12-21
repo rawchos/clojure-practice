29feee2f431b9340e64aada3dccc6d54f83155e9
Neal Scott <nealnmel@gmail.com>
2020-12-21 12:03:42 -0600

    Solution for part 2 of day 6 AoC 20

--- test/aoc_20/day_6_test.clj
+++ test/aoc_20/day_6_test.clj
@@ -11,3 +11,11 @@
              (d6/count-batch "ab\nac") => 3
              (d6/count-batch "a\na\na\na") => 1
              (d6/count-batch "b") => 1))
+
+(facts "about 'modified-count-batch'"
+       (fact "should count the batches correctly"
+             (d6/modified-count-batch "abc") => 3
+             (d6/modified-count-batch "a\nb\nc") => 0
+             (d6/modified-count-batch "ab\nac") => 1
+             (d6/modified-count-batch "a\na\na\na") => 1
+             (d6/modified-count-batch "b") => 1))
