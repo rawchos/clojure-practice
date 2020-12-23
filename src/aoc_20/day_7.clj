e93737108d1fd45e5cb09d619a7cf7fa563fabc6
Neal Scott <nealnmel@gmail.com>
2020-12-23 11:50:53 -0600

    Solution for part 2 of day 7 AoC 20

--- src/aoc_20/day_7.clj
+++ src/aoc_20/day_7.clj
@@ -61,6 +61,12 @@
   (when-let [[_ descr] (re-find #"\d+ (\w+ \w+) bag.*" rule)]
     (descr->keyword descr)))
 
+;; Note: Used in part-2. It's up here because we're re-using
+;; parse-rule in part-2.
+(defn add-rule-with-count [rule]
+  (when-let [[_ amount descr] (re-find #"(\d+) (\w+ \w+) bag.*" rule)]
+    [(descr->keyword descr) (Integer/parseInt amount)]))
+
 (defn in? [coll item]
   (some #(= item %) coll))
 
@@ -81,19 +87,65 @@
             (recur (rest bags)))
           false)))))
 
-(defn parse-rule [line]
+(defn parse-rule [add-rule-f line]
   (let [[container containees] (s/split line #" contain ")
         [_ descr] (re-find #"(\w+ \w+) bags" container)]
     {(descr->keyword descr)
      (into []
-           (->> (map add-rule (s/split containees #", "))
+           (->> (map add-rule-f (s/split containees #", "))
                 (remove nil?)))}))
 
 (defn part-1 [input]
-  (let [rules (into {} (map parse-rule input))]
+  (let [rules (into {} (map (partial parse-rule add-rule) input))]
     (->> (for [descr (keys rules)] (has-path? rules descr))
          (filter #(= true %))
          (count))))
 
 ; (part-1 (read-lines "day7-example-input.txt"))
 ; (part-1 (read-lines "day7-input.txt"))
+
+;; --- Part Two ---
+;; It's getting pretty expensive to fly these days - not because of ticket
+;; prices, but because of the ridiculous number of bags you need to buy!
+;; 
+;; Consider again your shiny gold bag and the rules from the above example:
+;; 
+;; faded blue bags contain 0 other bags.
+;; dotted black bags contain 0 other bags.
+;; vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted
+;;   black bags.
+;; dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted
+;;   black bags.
+;; So, a single shiny gold bag must contain 1 dark olive bag (and the 7
+;; bags within it) plus 2 vibrant plum bags (and the 11 bags within each
+;; of those): 1 + 1*7 + 2 + 2*11 = 32 bags!
+;; 
+;; Of course, the actual rules have a small chance of going several levels
+;; deeper than this example; be sure to count all of the bags, even if the
+;; nesting becomes topologically impractical!
+;; 
+;; Here's another example:
+;; 
+;; shiny gold bags contain 2 dark red bags.
+;; dark red bags contain 2 dark orange bags.
+;; dark orange bags contain 2 dark yellow bags.
+;; dark yellow bags contain 2 dark green bags.
+;; dark green bags contain 2 dark blue bags.
+;; dark blue bags contain 2 dark violet bags.
+;; dark violet bags contain no other bags.
+;; In this example, a single shiny gold bag must contain 126 other bags.
+;; 
+;; How many individual bags are required inside your single shiny gold bag?
+(defn count-bags [rules bag]
+  (let [bags (bag rules)]
+    (if (empty? bags)
+      0
+      (reduce +
+              (for [[next-bag amount] bags]
+                (+ (* amount (count-bags rules next-bag)) amount))))))
+
+(defn part-2 [input]
+  (let [rules (into {} (map (partial parse-rule add-rule-with-count) input))]
+    (count-bags rules :shiny-gold)))
+
+; (part-2 (read-lines "day7-input.txt"))
--- test/aoc_20/day_7_test.clj
+++ test/aoc_20/day_7_test.clj
@@ -17,8 +17,26 @@
 
 (facts "about 'parse-rule'"
        (fact "should return a map with description and other allowed types"
-             (d7/parse-rule "light red bags contain 1 bright white bag, 2 muted yellow bags.") => {:light-red [:bright-white :muted-yellow]}
-             (d7/parse-rule "bright white bags contain 1 shiny gold bag.") => {:bright-white [:shiny-gold]}
-             (d7/parse-rule "dotted black bags contain no other bags.") => {:dotted-black []}))
+             (d7/parse-rule d7/add-rule "light red bags contain 1 bright white bag, 2 muted yellow bags.") => {:light-red [:bright-white :muted-yellow]}
+             (d7/parse-rule d7/add-rule "bright white bags contain 1 shiny gold bag.") => {:bright-white [:shiny-gold]}
+             (d7/parse-rule d7/add-rule "dotted black bags contain no other bags.") => {:dotted-black []})
+       (fact "should return a map with description and count if using add-rule-with-count"
+             (d7/parse-rule d7/add-rule-with-count "light red bags contain 1 bright white bag, 2 muted yellow bags.") => {:light-red [[:bright-white 1] [:muted-yellow 2]]}
+             (d7/parse-rule d7/add-rule-with-count "bright white bags contain 1 shiny gold bag.") => {:bright-white [[:shiny-gold 1]]}
+             (d7/parse-rule d7/add-rule-with-count "dotted black bags contain no other bags.") => {:dotted-black []}))
+
+(let [rules {:vibrant-plum [[:faded-blue 5] [:dotted-black 6]]
+             :light-red [[:bright-white 1] [:muted-yellow 2]]
+             :shiny-gold [[:dark-olive 1] [:vibrant-plum 2]]
+             :dark-orange [[:bright-white 3] [:muted-yellow 4]]
+             :bright-white [[:shiny-gold 1]]
+             :muted-yellow [[:shiny-gold 2] [:faded-blue 9]]
+             :faded-blue []
+             :dark-olive [[:faded-blue 3] [:dotted-black 4]]
+             :dotted-black []}]
+  (fact "'count-bags' should accurately count the number of bags required"
+        (d7/count-bags rules :shiny-gold) => 32
+        (d7/count-bags rules :faded-blue) => 0
+        (d7/count-bags rules :dark-orange) => 406))
 
 
