0d2ec4dff45082beaab903bc55e1e501a3f92279
Neal Scott <nealnmel@gmail.com>
2020-12-24 10:37:21 -0600

    Solution for part 1 of day 8 AoC 20

---
+++ test/aoc_20/day_8_test.clj
@@ -0,0 +1,36 @@
+(ns aoc-20.day-8-test
+  (:require [midje.sweet :refer [fact
+                                 facts
+                                 =>]]
+            [aoc-20.day-8 :as d8]))
+
+(facts "about 'process-instruction'"
+       (let [state {:instruction-line       20
+                    :processed-instructions [0]
+                    :accumulator            10}]
+         (fact "should process jmp instructions"
+               (d8/process-instruction state "jmp -4") => {:instruction-line 16
+                                                           :processed-instructions [0 20]
+                                                           :accumulator 10}
+               (d8/process-instruction state "jmp +5") => {:instruction-line 25
+                                                           :processed-instructions [0 20]
+                                                           :accumulator 10})
+         (fact "should process nop instructions"
+               (d8/process-instruction state "nop +0") => {:instruction-line 21
+                                                           :processed-instructions [0 20]
+                                                           :accumulator 10}
+               (d8/process-instruction state "nop +4") => {:instruction-line 21
+                                                           :processed-instructions [0 20]
+                                                           :accumulator 10})
+         (fact "should process acc instructions"
+               (d8/process-instruction state "acc +5") => {:instruction-line 21
+                                                           :processed-instructions [0 20]
+                                                           :accumulator 15}
+               (d8/process-instruction state "acc -5") => {:instruction-line 21
+                                                           :processed-instructions [0 20]
+                                                           :accumulator 5})))
+
+(facts "about 'in?'"
+       (fact "should verify whether an item is in a collection"
+             (d8/in? [0 1 2 4] 3) => false
+             (d8/in? [0 1 2 4] 1) => true))
