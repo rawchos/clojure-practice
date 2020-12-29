a186d4bf626cfc86f8a553185d849822f6cb8e3f
Neal Scott <nealnmel@gmail.com>
2020-12-29 12:25:15 -0600

    Solution for part 2 of day 11 AoC 20

--- src/aoc_20/day_11.clj
+++ src/aoc_20/day_11.clj
@@ -26,7 +26,7 @@
   (-> (pad-seats (read-lines filename))
       (pad)))
 
-(defn adjacents [seat-map row column]
+(defn adjacents [seat-map [row column]]
   (let [previous-row (nth seat-map (dec row))
         this-row     (nth seat-map row)
         next-row     (nth seat-map (inc row))]
@@ -37,14 +37,14 @@
        (nth this-row (inc column))]
       (subvec (vec next-row) (dec column) (+ column 2))))))
 
-(defn adjust-seat [seat adjacents]
+(defn adjust-seat [seat adjacents max-occupied]
   (let [occupieds (-> (filter #(= "#" %) adjacents)
                       (count))]
     (if (= "L" seat)
       (if (pos? occupieds) "L" "#")
-      (if (>= occupieds 4) "L" "#"))))
+      (if (>= occupieds max-occupied) "L" "#"))))
 
-(defn adjust-seating [seat-map]
+(defn adjust-seating [seat-map adjacents-f max-occupied]
   (let [row-count (count seat-map)
         col-count (count (first seat-map))]
     (for [row (range row-count)]
@@ -53,7 +53,9 @@
                                 (nth column))]
                    (if (= "." seat)
                      seat
-                     (adjust-seat seat (adjacents seat-map row column)))))))))
+                     (adjust-seat seat
+                                  (adjacents-f seat-map [row column])
+                                  max-occupied))))))))
 
 (defn count-occupieds [row]
   (-> (filter #(= "#" %) row)
@@ -61,10 +63,62 @@
 
 (defn part-1 [seat-map]
   (loop [current-seat-map seat-map]
-    (let [adjusted-seat-map (adjust-seating current-seat-map)]
+    (let [adjusted-seat-map (adjust-seating current-seat-map adjacents 4)]
       (if (= current-seat-map adjusted-seat-map)
         (reduce + (map count-occupieds adjusted-seat-map))
         (recur adjusted-seat-map)))))
 
 ; (part-1 (pad-seat-map "day11-example-input.txt"))
 ; (part-1 (pad-seat-map "day11-input.txt"))
+
+(defn get-first-seat [seat-map direction [start-row start-column]]
+  (let [max-rows (count seat-map)
+        max-cols (count (first seat-map))]
+    (if (#{:W :E} direction)
+      (loop [columns (if (= :E direction)
+                       (range (inc start-column) max-cols)
+                       (reverse (range start-column)))
+             row start-row]
+        (if-let [column (first columns)]
+          (let [seat (-> (nth seat-map row)
+                         (nth column))]
+            (if (= "." seat)
+              (recur (rest columns) row)
+              seat))
+          nil))
+      (loop [rows (if (#{:NW :N :NE} direction)
+                    (reverse (range start-row))
+                    (range (inc start-row) max-rows))
+             prev-column start-column]
+        (if-let [row (first rows)]
+          (let [column (cond
+                         (#{:NW :SW} direction) (dec prev-column)
+                         (#{:NE :SE} direction) (inc prev-column)
+                         (#{:N :S} direction) prev-column)]
+            (if (and (pos? column)
+                     (< column max-cols))
+              (let [seat (-> (nth seat-map row)
+                             (nth column))]
+                (if (= "." seat)
+                  (recur (rest rows) column)
+                  seat))
+              nil))
+            nil)))))
+
+(def directions #{:NW :N :NE :E :SE :S :SW :W})
+
+(defn new-adjacents [seat-map coords]
+  (for [direction directions]
+    (get-first-seat seat-map direction coords)))
+
+(defn part-2 [seat-map]
+  (loop [current-seat-map seat-map]
+    (let [adjusted-seat-map (adjust-seating current-seat-map
+                                            new-adjacents
+                                            5)]
+      (if (= current-seat-map adjusted-seat-map)
+        (reduce + (map count-occupieds adjusted-seat-map))
+        (recur adjusted-seat-map)))))
+
+; (part-2 (pad-seat-map "day11-example-input.txt"))
+; (part-2 (pad-seat-map "day11-input.txt"))
