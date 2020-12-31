2a4fc03eac0ff305388303a947ee7a66a1e81dc7
Neal Scott <nealnmel@gmail.com>
2020-12-31 15:04:22 -0600

    Split input into indexed pairs for part 2 day 13

--- src/aoc_20/day_13.clj
+++ src/aoc_20/day_13.clj
@@ -25,5 +25,24 @@
     (-> (- earliest goal)
         (* bus-id))))
 
-(part-1 (read-lines "day13-example-input.txt"))
-(part-1 (read-lines "day13-input.txt"))
+; (part-1 (read-lines "day13-example-input.txt"))
+; (part-1 (read-lines "day13-input.txt"))
+
+;; -- Part 2 --
+
+; (def example "7,13,x,x,59,x,31,19")
+; (def example2 "13,x,x,41,x,x,x,x,x,x,x,x,x,997,x,x,x,x,x,x,x,23,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,x,x,x,x,x,29,x,619,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,17")
+; (def my-goal (Integer/parseInt "1000390"))
+
+(defn index-pairs [input]
+  (->> (map-indexed (fn [idx num]
+                    (when-not (= "x" num)
+                      [idx (Integer/parseInt num)])) (s/split input #","))
+       (remove nil?)))
+
+(defn part-2 [input]
+  (let [pairs (index-pairs (second input))]
+    pairs))
+
+
+(part-2 (read-lines "day13-example-input.txt"))
