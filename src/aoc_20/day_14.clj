8c3572806d2b7b475699c67b118635fdcf83040a
Neal Scott <nealnmel@gmail.com>
2021-01-04 12:43:15 -0600

    Solution for part 2 of day 14 AoC 20

--- src/aoc_20/day_14.clj
+++ src/aoc_20/day_14.clj
@@ -1,5 +1,6 @@
 (ns aoc-20.day-14
-  (:require [aoc-20.util :as util]))
+  (:require [aoc-20.util :as util]
+            [clojure.string :as s]))
 
 (defn read-lines [filename]
   (util/read-lines filename))
@@ -11,10 +12,13 @@
         pad (apply str (repeat num "0"))]
     (format "%s%s" pad s)))
 
+(defn pad-value [v]
+  (-> (Integer/parseInt v)
+      (Integer/toBinaryString)
+      (pad-zeros)))
+
 (defn mask-value [mask value]
-  (let [padded-value (-> (Integer/parseInt value)
-                         (Integer/toBinaryString)
-                         (pad-zeros))
+  (let [padded-value (pad-value value)
         pairs (->> (interleave mask padded-value)
                    (map str)
                    (partition 2))]
@@ -41,3 +45,58 @@
 
 ; (part-1 (read-lines "day14-example-input.txt"))
 ; (part-1 (read-lines "day14-input.txt"))
+
+;; -- Part 2 --
+
+(defn has-x? [address]
+  (some? (re-find #"X" address)))
+
+(defn convert-x [address]
+  (flatten
+   (for [new-address [(s/replace-first address #"X" "0")
+                      (s/replace-first address #"X" "1")]]
+     (if (has-x? new-address)
+       (convert-x new-address)
+       new-address))))
+
+(defn get-addresses [address]
+  (if (has-x? address)
+    (convert-x address)
+    [address]))
+
+;; - If the bitmask bit is 0, the corresponding memory address bit is
+;;   unchanged.
+;; - If the bitmask bit is 1, the corresponding memory address bit is
+;;   overwritten with 1.
+;; - If the bitmask bit is X, the corresponding memory address bit is
+;;   floating. (will replace with both 0 and 1)
+(defn mask-address [mask address]
+  (let [padded-value (pad-value address)
+        pairs (->> (interleave mask padded-value)
+                   (map str)
+                   (partition 2))]
+    (apply str (map (fn [[bit-mask bit]]
+                      (if (or (= "X" bit-mask)
+                              (= "1" bit-mask))
+                        bit-mask
+                        bit)) pairs))))
+
+(defn p2-process-instruction [{:keys [mask] :as state} instruction]
+  (if-let [[_ new-mask] (re-matches #"^mask = (\w+)$" instruction)]
+    (assoc state :mask new-mask)
+    (let [[_ slot value] (re-matches #"^mem\[(\d+)\] = (\d+)$" instruction)
+          masked-address (mask-address mask slot)
+          padded-value (pad-value value)]
+      (reduce (fn [state address]
+                (assoc-in state [:memory address] padded-value))
+              state
+              (get-addresses masked-address)))))
+
+(defn part-2 [input]
+  (->> (reduce p2-process-instruction {} input)
+       (:memory)
+       (vals)
+       (reduce add-binary-string 0)))
+
+; (part-2 (read-lines "day14-example-input2.txt"))
+; (part-2 (read-lines "day14-input.txt"))
--- test/aoc_20/day_14_test.clj
+++ test/aoc_20/day_14_test.clj
@@ -8,3 +8,28 @@
        (fact "should convert a binary string and add it"
              (d14/add-binary-string 0 "1001001") => 73
              (d14/add-binary-string 2 "000000000000000000000000000000001011") => 13))
+
+(facts "about 'mask-value'"
+       (fact "should use the mask to mask the value"
+             (d14/mask-value         "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"
+                             "11") ; "000000000000000000000000000000001011")
+                             =>      "000000000000000000000000000001001001"))
+
+(facts "about 'has-x?'"
+       (fact "should be true if there are any Xs in the string"
+             (d14/has-x? "00X10") => true
+             (d14/has-x? "X0010") => true
+             (d14/has-x? "0001X") => true)
+       (fact "should be false if there aren't any Xs in the string"
+             (d14/has-x? "00010") => false))
+
+(facts "about 'convert-x'"
+       (fact "should convert the Xs to both 0 and 1 versions"
+             (d14/convert-x "00X00") => '("00000" "00100")
+             (d14/convert-x "00X00X") => '("000000" "000001" "001000" "001001")))
+
+(facts "about 'mask-address'"
+       (fact "should mask the address based on the mask provided"
+             (d14/mask-address         "000000000000000000000000000000X1001X"
+                               "42") ; "000000000000000000000000000000101010")
+                               =>      "000000000000000000000000000000X1101X"))
