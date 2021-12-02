fc324c05e0222b065ab33318ddd1c5f624dbeb10
Neal Scott <nealnmel@gmail.com>
2021-12-02 14:51:59 -0600

    Day 1 of Advent of Code 2021 - no tests yet

---
+++ src/aoc_21/day_01.clj
@@ -0,0 +1,39 @@
+(ns aoc-21.day-01
+  (:require [aoc-utils.core :as util]))
+
+(def sample-readings [199 200 208 210 200 207 240 269 260 263])
+
+(defn get-input []
+  (->> (util/read-lines "21" "day1.txt")
+       (map util/to-int)))
+
+(defn p2-input []
+  (->> (get-input)
+       (partition 3 1)
+       (map #(reduce + %))))
+
+(defn part-1 [readings]
+  (-> (reduce (fn [{:keys [previous-reading] :as state} reading]
+                (let [new-state (assoc state :previous-reading reading)]
+                  (cond
+                    (< previous-reading reading) (update-in new-state [:increased] inc)
+                    (> previous-reading reading) (update-in new-state [:decreased] inc)
+                    :else new-state)))
+              {:previous-reading (first readings)
+               :increased        0
+               :decreased        0}
+              (rest readings))
+      :increased))
+
+(comment
+  (util/read-lines "21" "day1.txt")
+  (part-1 sample-readings)
+
+  (part-1 (get-input)) ; 1482
+  (part-1 (p2-input))
+
+  (def my-input (get-input))
+  (->> (partition 3 1 sample-readings)
+       (map #(reduce + %))
+       (part-1))
+  )
--- src/aoc_utils/core.clj
+++ src/aoc_utils/core.clj
@@ -2,16 +2,20 @@
   (:require [clojure.java.io :as io]))
 
 (defn read-lines 
-  "Reads in an input file from src/aoc_20/inputs/ and creates a sequence."
-  [input-file]
-  (with-open [rdr (io/reader (str "src/aoc_20/inputs/" input-file))]
-    (doall (line-seq rdr))))
+  "Reads in an input file from src/aoc_[year]/inputs/ and creates a sequence."
+  ([input-file]
+   (read-lines "20" input-file))
+  ([aoc-year input-file]
+   (with-open [rdr (io/reader (str "src/aoc_" aoc-year "/inputs/" input-file))]
+     (doall (line-seq rdr)))))
 
 (defn read-str
   "This function reads the entire input into a string that we can
    then parse through, rather than reading each line."
-  [filename]
-  (slurp (str "src/aoc_20/inputs/" filename)))
+  ([filename]
+   (read-str "20" filename))
+  ([aoc-year filename]
+   (slurp (str "src/aoc_" aoc-year "/inputs/" filename))))
 
 (defn to-int 
   "Convenience method for coercing a string to an int."
