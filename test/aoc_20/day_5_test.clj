f8a7f2943ec939a30fbce90da3f082ee56c7e48a
Neal Scott <neal.scott@mckesson.com>
2024-12-20 15:25:29 -0600

    Updated a couple more aoc_20 tests

--- test/aoc_20/day_5_test.clj
+++ test/aoc_20/day_5_test.clj
@@ -1,44 +1,42 @@
 (ns aoc-20.day-5-test
-  (:require [midje.sweet :refer [fact
-                                 facts
-                                 =>]]
+  (:require [clojure.test :refer [deftest is testing]]
             [aoc-20.day-5 :as d5]
             [clojure.string :as s]))
 
-(facts "about 'find-row'"
-       (fact "should parse the row correctly"
-             (d5/find-row (s/split "BBFFBBF" #"")) => 102
-             (d5/find-row (s/split "BFFFBBF" #"")) => 70
-             (d5/find-row (s/split "FFFBBBF" #"")) => 14
-             (d5/find-row (s/split "FBFBBFF" #"")) => 44))
+(deftest find-row-test
+       (testing "should parse the row correctly"
+             (is (= (d5/find-row (s/split "BBFFBBF" #"")) 102))
+             (is (= (d5/find-row (s/split "BFFFBBF" #"")) 70))
+             (is (= (d5/find-row (s/split "FFFBBBF" #"")) 14))
+             (is (= (d5/find-row (s/split "FBFBBFF" #"")) 44))))
 
-(facts "about 'find-seat'"
-       (fact "should parse the seat correctly"
-             (d5/find-seat (s/split "RRR" #"")) => 7
-             (d5/find-seat (s/split "RLL" #"")) => 4
-             (d5/find-seat (s/split "RLR" #"")) => 5))
+(deftest find-seat-test
+       (testing "should parse the seat correctly"
+             (is (= (d5/find-seat (s/split "RRR" #"")) 7))
+             (is (= (d5/find-seat (s/split "RLL" #"")) 4))
+             (is (= (d5/find-seat (s/split "RLR" #"")) 5))))
 
-(facts "about 'seat-id'"
-       (fact "should determine the seat id correctly"
-             (d5/seat-id 70 7) => 567
-             (d5/seat-id 14 7) => 119
-             (d5/seat-id 102 4) => 820))
+(deftest seat-id-test
+       (testing "should determine the seat id correctly"
+             (is (= (d5/seat-id 70 7) 567))
+             (is (= (d5/seat-id 14 7) 119))
+             (is (= (d5/seat-id 102 4) 820))))
 
-(facts "about 'parse-ticket'"
-       (fact "should parse the ticket correctly"
-             (d5/parse-ticket "BFFFBBFRRR") => {567 [70 7]}
-             (d5/parse-ticket "FFFBBBFRRR") => {119 [14 7]}
-             (d5/parse-ticket "BBFFBBFRLL") => {820 [102 4]}
-             (d5/parse-ticket "FBFBBFFRLR") => {357 [44 5]}))
+(deftest parse-ticket-test
+       (testing "should parse the ticket correctly"
+             (is (= (d5/parse-ticket "BFFFBBFRRR") {567 [70 7]}))
+             (is (= (d5/parse-ticket "FFFBBBFRRR") {119 [14 7]}))
+             (is (= (d5/parse-ticket "BBFFBBFRLL") {820 [102 4]}))
+             (is (= (d5/parse-ticket "FBFBBFFRLR") {357 [44 5]}))))
 
-(facts "about 'my-seat?'"
-       (fact "should be true if the id +1 and -1 exist"
-             (d5/my-seat? {2 [1 2]
+(deftest my-seat?-test
+       (testing "should be true if the id +1 and -1 exist"
+             (is (true? (d5/my-seat? {2 [1 2]
                            3 [3 4]
-                           4 [5 6]} 3) => true)
-       (fact "should be false if either of the +1 or the -1 don't exist"
-             (d5/my-seat? {3 [3 4]
-                           4 [5 6]} 3) => false
-             (d5/my-seat? {2 [1 2]
-                           3 [3 4]} 3) => false
-             (d5/my-seat? {3 [3 4]} 3) => false))
+                           4 [5 6]} 3))))
+       (testing "should be false if either of the +1 or the -1 don't exist"
+             (is (false? (d5/my-seat? {3 [3 4]
+                           4 [5 6]} 3)))
+             (is (false? (d5/my-seat? {2 [1 2]
+                           3 [3 4]} 3)))
+             (is (false? (d5/my-seat? {3 [3 4]} 3)))))
--- test/aoc_20/day_6_test.clj
+++ test/aoc_20/day_6_test.clj
@@ -1,21 +1,19 @@
 (ns aoc-20.day-6-test
-  (:require [midje.sweet :refer [fact
-                                 facts
-                                 =>]]
+  (:require [clojure.test :refer [deftest is testing]]
             [aoc-20.day-6 :as d6]))
 
-(facts "about 'count-batch'"
-       (fact "should count batches correctly"
-             (d6/count-batch "abc") => 3
-             (d6/count-batch "a\nb\nc") => 3
-             (d6/count-batch "ab\nac") => 3
-             (d6/count-batch "a\na\na\na") => 1
-             (d6/count-batch "b") => 1))
+(deftest count-batch-test
+       (testing "should count batches correctly"
+             (is (= (d6/count-batch "abc") 3))
+             (is (= (d6/count-batch "a\nb\nc") 3))
+             (is (= (d6/count-batch "ab\nac") 3))
+             (is (= (d6/count-batch "a\na\na\na") 1))
+             (is (= (d6/count-batch "b") 1))))
 
-(facts "about 'modified-count-batch'"
-       (fact "should count the batches correctly"
-             (d6/modified-count-batch "abc") => 3
-             (d6/modified-count-batch "a\nb\nc") => 0
-             (d6/modified-count-batch "ab\nac") => 1
-             (d6/modified-count-batch "a\na\na\na") => 1
-             (d6/modified-count-batch "b") => 1))
+(deftest modified-count-batch-test
+       (testing "should count the batches correctly"
+             (is (= (d6/modified-count-batch "abc") 3))
+             (is (= (d6/modified-count-batch "a\nb\nc") 0))
+             (is (= (d6/modified-count-batch "ab\nac") 1))
+             (is (= (d6/modified-count-batch "a\na\na\na") 1))
+             (is (= (d6/modified-count-batch "b") 1))))
