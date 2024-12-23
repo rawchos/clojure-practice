8000c26fe3363ff0ef46934af84941a6896c9b5b
Neal Scott <neal.scott@mckesson.com>
2024-12-23 12:37:30 -0600

    Convert interview_questions tests

--- test/interview_questions/stock_profit_test.clj
+++ test/interview_questions/stock_profit_test.clj
@@ -1,16 +1,14 @@
 (ns interview-questions.stock-profit-test
   (:require [interview-questions.stock-profit :as sp]
-            [midje.sweet :refer [fact
-                                 facts
-                                 =>]]))
+            [clojure.test :refer [deftest is testing]]))
 
-(facts "about 'at-least-2?'"
-       (fact "should verify whether or not there are at least 2 of a number"
-             (sp/at-least-2? [6 12 3 9 3 5 1] 3) => true
-             (sp/at-least-2? [6 12 3 9 3 5 1] 6) => false))
+(deftest at-least-2?-test
+       (testing "should verify whether or not there are at least 2 of a number"
+             (is (true? (sp/at-least-2? [6 12 3 9 3 5 1] 3)))
+             (is (false? (sp/at-least-2? [6 12 3 9 3 5 1] 6)))))
 
-(facts "about 'stock-profit'"
-       (fact "should find the correct number of pairs with duplicate numbers"
-             (sp/stock-profit [1 3 46 1 3 9] 47) => 1)
-       (fact "should find the correct number of pairs when only 1 number of half the target"
-             (sp/stock-profit [6 12 3 9 3 5 1] 12) => 2))
+(deftest stock-profit-test
+       (testing "should find the correct number of pairs with duplicate numbers"
+             (is (= (sp/stock-profit [1 3 46 1 3 9] 47) 1)))
+       (testing "should find the correct number of pairs when only 1 number of half the target"
+             (is (= (sp/stock-profit [6 12 3 9 3 5 1] 12) 2))))
